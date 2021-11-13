3d7368f14eeb90bc87411ca7dc69490b1cf486f3
dahuin-1 <dawhite01@naver.com>
2021-11-14 02:43:54 +0900

    Minumum Spanning Tree

---
+++ src/Grade4/Algorithm/week10/spanningtree/Prim.java
@@ -0,0 +1,67 @@
+package Grade4.Algorithm.week10.spanningtree;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Prim extends WeightedGraphInList {
+    int [] d  ;   // distance;
+    int r = -1;   // start node
+    HashSet<String> S, V ;
+
+    public Prim(int max) {
+        super(max);
+    }
+    public void init(String start) {
+        d = new int [maxNumber];
+        S = new HashSet<>();
+        V = new HashSet<>();
+
+        V.addAll(vertices);
+        r = vertices.indexOf(start);
+        Arrays.fill(d, 9999);
+        d[r]=0;
+    }
+
+    public void MST() {
+
+        while(S.size()<maxNumber) {
+            String u = extractMin(diff(V,S));  // diff(V,S) == V-S
+            S.add(u);
+            System.out.println(">>> "+u+" is selected.");
+            for (String v : adjacent(u)) {  // L(u) == adjacent(u)
+                HashSet<String> temp = diff(V,S);
+                int wuv = getWeight(u, v);
+                int dv = d[vertices.indexOf(v)];
+                if (temp.contains(v) &&  wuv<dv)
+                    d[vertices.indexOf(v)] = wuv ;
+            }
+        }
+        for (int i=0; i<maxNumber; i++)
+            System.out.print(vertices.get(i)+"("+d[i]+")");
+        System.out.println();
+
+    }
+
+    private int getWeight(String u, String v) {
+        return getEdge(u, v).weight;
+    }
+    private HashSet<String> diff(HashSet<String> s1, HashSet<String> s2) {
+        HashSet<String> result = s1;
+        for (String s : s2)
+            result.remove(s);
+        return result;
+    }
+
+    private String extractMin(HashSet<String> diff) {
+        String minVertex = null;
+        int min = 9999;;
+        for (String s : diff) {
+            if (d[vertices.indexOf(s)] < min) {
+                minVertex = s;
+                min = d[vertices.indexOf(s)];
+            }
+        }
+        return minVertex;
+    }
+
+}
